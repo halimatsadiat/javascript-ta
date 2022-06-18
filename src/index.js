a14974b6821969a404acacc5215173b471611d30
halimatsadiat <66258094+halimatsadiat@users.noreply.github.com>
2022-06-18 23:38:11 +0100

    Added the prime number function
    Added the solution to the prime number

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 // complete the function
 function prime (num) {
   // code goes here
+  if(num <= 1){
+      return false
+    } else if (num === 2){
+        return true
+    } else if ( num > 2){
+        for(let i = 2; i < num; i++){
+            if (num % i === 0){
+                return false
+                break
+            } 
+        } return true
+    }
 }
 
 function solution (arg) {
